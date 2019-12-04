ed45eebf4f375e14436f71466b70e292d99a0f14
Hal Peters <geohalbert@gmail.com>
2019-12-04 16:24:49 -0600

    including browser history

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,17 +1,67 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { NavBar, Main } from "../components";
+import { history } from "../history";
+import { alertActions } from "../actions";
+
+import { NavBar } from "../components";
+import {
+  QuakesList,
+  QuakesInsert,
+  QuakesUpdate,
+  USGSList,
+  USGSQuery,
+  HomeView,
+  USGSQuakeView
+} from "../views";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Main />
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    history.listen((location, action) => {
+      // clear alert on location change
+      this.props.clearAlerts();
+    });
+  }
+
+  render() {
+    const { alert } = this.props;
+    return (
+      <div className="container">
+        <div className="col-sm-8 col-sm-offset-2">
+          {alert.message && (
+            <div className={`alert ${alert.type}`}>{alert.message}</div>
+          )}
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={HomeView} />
+              <Route exact path="/USGS" component={USGSQuery} />
+              <Route path="/USGS/:id" component={USGSQuakeView} />
+              <Route path="/USGS/list" exact component={USGSList} />
+              <Route path="/quakes" exact component={QuakesList} />
+              <Route path="/quakes/create" exact component={QuakesInsert} />
+              <Route path="/quakes/update/:id" exact component={QuakesUpdate} />
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapState(state) {
+  const { alert } = state;
+  return { alert };
+}
+
+const actionCreators = {
+  clearAlerts: alertActions.clear
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
